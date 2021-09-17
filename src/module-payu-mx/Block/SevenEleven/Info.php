fd013285e0d44016db87056caac468a2b8af6687
dholi <comunidade@dholi.dev>
2021-09-17 00:43:11 -0300

    Clear blocks

--- src/module-payu-mx/Block/SevenEleven/Info.php
+++ src/module-payu-mx/Block/SevenEleven/Info.php
@@ -15,9 +15,6 @@ declare(strict_types=1);
 
 namespace Eloom\PayUMx\Block\SevenEleven;
 
-use Magento\Framework\Phrase;
-use Magento\Payment\Block\ConfigurableInfo;
-
 class Info extends \Eloom\PayU\Block\Info {
 	
 	public function getPaymentLink() {
