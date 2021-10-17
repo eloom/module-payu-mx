d4cb5ae95041281df9252b7accde856d9c5953a6
dholi <comunidade@dholi.dev>
2021-10-17 16:46:10 -0300

    v 1.0.2

--- src/module-payu-mx/Gateway/Request/Payment/OxxoDataBuilder.php
+++ src/module-payu-mx/Gateway/Request/Payment/OxxoDataBuilder.php
@@ -3,10 +3,10 @@
 * 
 * PayU Mexico para Magento 2
 * 
-* @category     Ã©lOOm
+* @category     elOOm
 * @package      Modulo PayUMx
 * @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
-* @version      1.0.1
+* @version      1.0.2
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
